83a6fcb932bdd569ca3977486114c5fd55f3e70a
ahtchow <54782402+ahtchow@users.noreply.github.com>
2022-01-29 20:39:17 -0500

    Update 189_Rotate_Array.cpp

--- Arrays/189_Rotate_Array.cpp
+++ Arrays/189_Rotate_Array.cpp
@@ -1,50 +1,32 @@
-/* Rotate Array 
-
-[1,2,3,4,5,6,7], k = 3
- 
-[5,6,7,1,2,3,4]
- 
-
-
-*/
-
 class Solution {
 public:
     
-    void rotatePointer(vector<int>& nums, int k) {
-        
-        /* Using extra memory O(n), O(n) */
-        vector<int> temp = nums;
-        int x = k % nums.size(); // Idx in which the rotated array starts with
-        cout << x << endl;
-        for(int i = 0; i < nums.size(); i++){
-            int new_idx = (x+i)%nums.size();
-            nums[new_idx] = temp[i];
+    /*
+    
+    Optimal: O(n) time, O(1) space
+    [1,2,3,4,5,6,7], k =1
+    [7,6,5,4,3,2,1], reverse entire nums
+    [7,6,5|4,3,2,1] , mark before kth indx
+    [5,6,7|1,2,3,4] , reverse from both sides
+    
+    */
+    
+    
+    void reverse(vector<int> &nums, int i, int j){
+        while(i < j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
         }
-        
     }
+    
     void rotate(vector<int>& nums, int k) {
-        
-        /* Take Advantage of Reverse O(n), O(1) */
-        
-        /*
-            [1,2,3,4,5,6,7], k = 3
-            
-            1. Reverse Entire Array
-            [7,6,5,4,3,2,1], k = 3
-            
-            2. Reverse Two Subarrays, marked by k
-            [5,6,7][1,2,3,4]
-            
-        
-        */
-        
-        int x = k % nums.size();
-        reverse(nums.begin(), nums.end());
-        /*Reverse sub_arrays */
-        reverse(nums.begin(), nums.begin()+x);
-        reverse(nums.begin()+x, nums.end());
+        int a = k % nums.size();
+        reverse(nums, 0, nums.size()-1);
+        reverse(nums, 0, a-1);
+        reverse(nums, a, nums.size()-1);
         
     }
-
 };
